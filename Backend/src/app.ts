0136441f32f46616503fcff5d246520e0117248c
Archie Jain <archie.jain@tothenew.com>
2026-09-16 14:53:39 +0530

    updated port and start script

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -12,7 +12,7 @@ import swaggerDocs from "./swagger.ts";
 import "./passport.ts";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 const frontendUrl = process.env.FRONTEND_URL ?? "http://localhost:3001";
 
 app.use((req, res, next) => {
